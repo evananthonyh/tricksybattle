b241aa89ef6342fab17afb44733a0255580a62e8
Evan H <evan@evans-MacBook-Pro.local>
2025-05-06 11:56:13 -0400

    Deal 8 cards to each player and initialize scores

--- tricksybattle1.py
+++ tricksybattle1.py
@@ -8,3 +8,12 @@ values = {rank: i+1 for i, rank in enumerate(ranks)}
 # Create and shuffle the deck
 deck = [((suit, rank), values[rank]) for suit in suits for rank in ranks]
 random.shuffle(deck)
+
+# Deal 8 cards to each player
+player1 = deck[:8]
+player2 = deck[8:16]
+deck = deck[16:]
+
+# Start scores at 0
+player1_score = 0
+player2_score = 0
